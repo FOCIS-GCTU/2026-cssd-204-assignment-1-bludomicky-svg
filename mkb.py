2d0275b2e59b8dab4d2e93fd30ea3a8d37761015
bludomicky-svg <bludomicky@gmail.com>
2026-05-31 19:22:55 +0000

    Add files via upload

---
+++ mkb.py
@@ -0,0 +1,37 @@
+
+
+
+
+def main():
+
+
+    #The display of the first initials of the name Michael Kwesi Bludo
+    print("\n")
+    print( "...MKB")
+    print("\n")
+    print("...MMMM....MMMM........KK........KK........BBBBBBBBBB..")
+    print("...MMMM....MMMM........KK......KK..........BB........BB")
+    print("...MM.MM.MM..MM........KK....KK............BB........BB")
+    print("...MM...MM...MM........KK..KK..............BB........BB")
+    print("...MM...MM...MM........KKKK................BB.BB..BB...")
+    print("...MM........MM........KKKKKK..............BB........BB")
+    print("...MM........MM........KK..kk..............BB........BB")
+    print("...MM........MM........KK....KK............BB........BB")
+    print("...MM........MM...**...KK......KK.....**...BB........BB")
+    print("...MM........MM...**...KK........KK...**...BBBBBBBBBB..")
+
+    # File:mkb.py
+    # Description: Print out my initials.
+    #Assignment Number:1
+    #
+    #Name:<MICHAEL KWESI BLUDO>
+    #Student ID:<2425403103>
+    #Email:2425403103@gctu.edu.gh
+    #Grader: Augustus Buckman
+    #
+    #On my honor,Michael Kwesi Bludo,this programming assignment is my work
+    #and I have not provided this assignment to any other student.
+main()
+
+
+
